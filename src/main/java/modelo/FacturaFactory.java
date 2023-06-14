bccf762fb5645daa72de739a8a3274f7e6c4c07b
Tomás <55422247+TomasCiviero@users.noreply.github.com>
2023-06-14 14:55:04 -0300

    Update FacturaFactory.java

--- src/main/java/modelo/FacturaFactory.java
+++ src/main/java/modelo/FacturaFactory.java
@@ -5,10 +5,10 @@ import java.io.Serializable;
 public class FacturaFactory implements Serializable{
 
     public Factura crearFactura(String tipoFactura, int id) {
-        if(tipoFactura.equals("Jurídica")){
+        if(tipoFactura.equals("Juridica")){
             return new FacturaJuridica(id);
         }
-        else if(tipoFactura.equals("Física")){
+        else if(tipoFactura.equals("Fisica")){
             return new FacturaFisica(id);
         }
         return null;
