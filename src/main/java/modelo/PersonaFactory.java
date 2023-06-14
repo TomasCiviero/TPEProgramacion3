353d199b79364977e979484648483ab9f5cc1a13
Tomás <55422247+TomasCiviero@users.noreply.github.com>
2023-06-14 14:54:35 -0300

    Update PersonaFactory.java

--- src/main/java/modelo/PersonaFactory.java
+++ src/main/java/modelo/PersonaFactory.java
@@ -5,10 +5,10 @@ import java.io.Serializable;
 
 public class PersonaFactory implements Serializable{
     public Persona crearPersona(String tipoPersona, int id,int idFactura){
-        if(tipoPersona.equals("Jurídica")){
+        if(tipoPersona.equals("Juridica")){
             return new PersonaJuridica(id,idFactura);
         }
-        else if(tipoPersona.equals("Física")){
+        else if(tipoPersona.equals("Fisica")){
             return new PersonaFisica(id,idFactura);
         }
         return null;
